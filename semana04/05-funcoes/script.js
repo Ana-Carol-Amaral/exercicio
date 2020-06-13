e76575a0da0268a682a8ce0e884f580fbe17be36
Carol <carol@pop-os.localdomain>
2020-06-13 14:00:46 -0300

    funcoes

---
+++ semana04/05-funcoes/script.js
@@ -0,0 +1,58 @@
+'use strict'
+
+var acertos = 0;
+var erros = 0;
+
+var i = 0;
+
+while (i < 5) {
+    var meuNumero = prompt ('Digite um número de 0 e 10');
+
+    let sort = Math.round(Math.random() * 10);
+
+    console.log(sort);
+    
+    if (sort == meuNumero)
+        acertos++;
+    else
+        erros++;
+    i++;
+}
+
+document.write('<h1>Acertos:' + acertos + 'Erros: ' + erros + '</h1>');
+
+/*
+createElement('app', 10)
+createElement('outro', 5)
+
+function createElement(elemento, limite){
+    var app = document.getElementById(elemento);
+
+    var ul = document.createElement('ul');
+
+    for (let i = 0; i < limite; i++){
+    let li = document.createElement('li');
+    li.innerText = 'Número ';
+
+    let span = document.createElement('span');
+    let valor = (i + 1);
+    span.innerText = valor < 10 ? '0' + valor : valor;
+    span.style.fontWeight = 'bold';
+    span.style.color = 'green';
+    //span.setAttribute('style', 'font-weight: bold;');
+
+    li.appendChild(span);
+
+    ul.appendChild(li);
+    }
+
+app.appendChild(ul);
+}
+
+var resultado = minhaFuncao(5, 5);
+alert(resultado);
+
+function minhaFuncao(p1, p2) {
+
+    return p1 * p2
+}*/
